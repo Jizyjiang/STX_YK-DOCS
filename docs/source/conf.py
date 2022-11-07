1223226e355ee0ee8b3c65f6a22c9184ab25e60f
Jizyjiang <4638243+Jizyjiang@users.noreply.github.com>
2022-11-07 11:00:35 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = '音控室 专业手册'
+copyright = '2022'
+author = '蒋易银'
 
 release = '0.1'
 version = '0.1.0'
